5c625afe64a15908a41d4c3ab311dd8cbe37ea4b
Simone Staccone <simone.staccone@virgilio.it>
2023-05-27 12:13:01 +0200

    Restored with home made tests

---
+++ bookkeeper-http/servlet-http-server/src/test/java/org/apache/bookkeeper/http/servlet/BookieHttpServiceServletTest.java
@@ -0,0 +1,86 @@
+package org.apache.bookkeeper.http.servlet;
+
+import junit.framework.TestCase;
+
+public class BookieHttpServiceServletTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testDoGet() {
+    }
+
+    public void testGetLastModified() {
+    }
+
+    public void testDoHead() {
+    }
+
+    public void testDoPost() {
+    }
+
+    public void testDoPut() {
+    }
+
+    public void testDoDelete() {
+    }
+
+    public void testDoOptions() {
+    }
+
+    public void testDoTrace() {
+    }
+
+    public void testService() {
+    }
+
+    public void testTestService() {
+    }
+
+    public void testDestroy() {
+    }
+
+    public void testGetInitParameter() {
+    }
+
+    public void testGetInitParameterNames() {
+    }
+
+    public void testGetServletConfig() {
+    }
+
+    public void testGetServletContext() {
+    }
+
+    public void testGetServletInfo() {
+    }
+
+    public void testInit() {
+    }
+
+    public void testTestInit() {
+    }
+
+    public void testLog() {
+    }
+
+    public void testTestLog() {
+    }
+
+    public void testGetServletName() {
+    }
+
+    public void testTestService1() {
+        assertEquals(1,1);
+    }
+
+    public void testHttpServletParams() {
+    }
+
+    public void testHttpServerMethod() {
+    }
+}
